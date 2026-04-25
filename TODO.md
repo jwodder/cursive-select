1338b031a8bf690a5cebb3ab3b8a4dd61b871392
John T. Wodder II <git@varonathe.org>
2026-04-25 15:45:35 -0400

    Test long text

--- TODO.md
+++ TODO.md
@@ -1,6 +1,4 @@
 - Improve spacing around buttons
-- Test wrapping of long text
-
 - Keybindings to add:
     - `g` — jump to top
     - `G` — jump to bottom
@@ -10,11 +8,10 @@
     - `z` — go down a page
     - `Tab` — go to next selection list
     - `Shift`+`Tab` — go to previous selection list
-
 - Stylize selection list titles?
 - Add a cell of padding around the view area?
-- `Selector::Single`: Handle `default` being out of range for `options`?
 
+- `Selector::Single`: Handle `default` being out of range for `options`?
 - Allow setting a list/set of defaults for multi-selections
 - Better handling of calling `run()` with no selection lists?
 - Get rid of the shadow on the view?
@@ -22,6 +19,9 @@
 - Slight problem with scrolling: If you scroll to the bottom and then wrap
   around to the top, the first focusable item will be on top of the screen,
   cutting off the text for the first selection list.
+- Problem: Long single-selection lines are cut off, while long multi-selection
+  lines are wrapped
+    - Report as bug?
 
 - After next release of `cursive` after v0.21.1:
     - Use `Checkbox::labelled()`
--- src/main.rs
+++ src/main.rs
@@ -197,6 +197,37 @@ fn main() {
                 default: 5,
             },
         );
+    } else if arg.is_some_and(|s| s == "lorem") {
+        app.add(
+            "lorem",
+            Selector::Single {
+                title: "Lorem".to_owned(),
+                options: vec1![
+                    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod".into(),
+                    "tincidunt ut laoreet dolore magna aliquam erat volutpat.  Ut wisi enim ad minim veniam,".into(),
+                    "quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo".into(),
+                    "consequat.  Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse".into(),
+                    "molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan".into(),
+                    "et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore".into(),
+                    "te feugait nulla facilisi.  Nam liber tempor cum soluta nobis eleifend option congue".into(),
+                ],
+                default: 0,
+            }
+        );
+        app.add("ipsum",
+            Selector::Multi {
+                title: "Ipsum".to_owned(),
+                options: vec1![
+                    "nihil imperdiet doming id quod mazim placerat facer possim assum.  Typi non habent".into(),
+                    "claritatem insitam; est usus legentis in iis qui facit eorum claritatem.  Investigationes".into(),
+                    "demonstraverunt lectores legere me lius quod ii legunt saepius.  Claritas est etiam".into(),
+                    "processus dynamicus, qui sequitur mutationem consuetudium lectorum.  Mirum est notare".into(),
+                    "quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas".into(),
+                    "humanitatis per seacula quarta decima et quinta decima.  Eodem modo typi, qui nunc".into(),
+                    "nobis videntur parum clari, fiant sollemnes in futurum.".into(),
+                ],
+            }
+        );
     } else if arg.is_some_and(|s| s == "empty") {
     } else {
         app.add(
